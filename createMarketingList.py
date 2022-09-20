617ded286fa71b891aefdf6fae07c066ac162078
Johnny Bhojwani <johnnyvb@gmail.com>
2022-09-19 20:56:02 -0500

    initial commit

---
+++ createMarketingList.py
@@ -0,0 +1,56 @@
+# Read the file VendorList.csv into this program and create a dictionary of the customer's
+# full name, email address and phone number where the key is the full name of the customer
+# and the value is a dictionary where the keys are 'email' and 'phone' and the values
+# are the corresponding email address and phone number of the customer. 
+
+# Once the dictionary has been completed print it out. It shoud resemble what is shown
+# below (first 2 and last 2 elements shown only):
+
+#{'Tommie Goody': {'email': 'tgoody0@weather.com', 'phone': '809-992-7298'}, 
+# 'Obadiah Godfery': {'email': 'ogodfery1@a8.net', 'phone': '560-745-9361'}......
+# ..........'Kessiah Tynemouth': {'email': 'ktynemouthdu@ning.com', 'phone': '690-215-8097'}, 
+# 'Carmela Kaubisch': {'email': 'ckaubischdv@wikia.com', 'phone': '307-726-6526'}}
+
+
+# Using the dictionary, write the contents to a csv file. The output file shoud be exactly as
+# what is shown in the file - marketinglist.csv.
+# Name your file - marketinglistFINAL.csv
+
+
+import csv
+
+# open the vendorlist file
+
+
+# create a csv object from the file object
+
+
+# create an output file
+
+
+
+
+
+# create an empty dictionary
+
+
+
+# iterate through the csv object
+
+
+
+
+    # add the key-value pair to the dictionary
+
+
+
+# print the dictionary after the loop is finished
+
+
+
+# iternate through the dictionary and write to the output file
+
+
+
+# close your output file
+
